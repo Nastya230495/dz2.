b16edf31ecd1b57070d396e7d14ea171c7468671
Nastya <anastasic.curly@mail.ru>
2022-10-09 17:18:40 +0300

    Add task15

---
+++ task15/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear ();
+Console.WriteLine ("Введите любое число");
+int n = Convert.ToInt32 (Console.ReadLine());
+string m = Convert.ToString(n);
+if (m.Length >2)
+Console.WriteLine ($"Третья цифра: {m[2]}");
+else
+Console.WriteLine("Третьей цифры нет");
