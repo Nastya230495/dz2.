2ba93541ad1fc28bb1a899afb1e8e535f413019e
Nastya <anastasic.curly@mail.ru>
2022-10-09 17:14:42 +0300

    Add Task13

---
+++ task13/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear ();
+Console.WriteLine ("Введите цифру от 1 до 7");
+int n = Convert.ToInt32 (Console.ReadLine());
+while (n<1 || n>7)
+{
+    Console.Write ("Вы ошиблись. Введите цифру от 1 до 7");
+    n = Convert.ToInt32 (Console.ReadLine());
+}
+if (n<=5)
+    Console.WriteLine ("Нет");
+if (n>5)
+Console.WriteLine ("Да");
